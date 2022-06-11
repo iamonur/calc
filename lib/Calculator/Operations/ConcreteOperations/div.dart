d6aa6f2946fa7ba99b01d26e519afccfe9d4587d
Onur Tekik <otekik@gmail.com>
2022-06-11 22:32:17 +0300

    Fix naming

--- lib/Calculator/Operations/ConcreteOperations/div.dart
+++ lib/Calculator/Operations/ConcreteOperations/div.dart
@@ -2,8 +2,8 @@ import '../Interfaces/operations.dart';
 import '../Interfaces/double_operand_operation.dart';
 import '../Interfaces/number.dart';
 
-class Add extends DoubleOperandOperation {
-  Add(Operation op1, Operation op2) : super(op1, op2);
+class Div extends DoubleOperandOperation {
+  Div(Operation op1, Operation op2) : super(op1, op2);
 
   @override
   Operation operate() {
