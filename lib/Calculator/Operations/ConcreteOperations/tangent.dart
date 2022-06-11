46f3508012d605bd63c5c313fb63a178f99cbe3b
Onur Tekik <otekik@gmail.com>
2022-06-11 22:46:37 +0300

    Add tangent

---
+++ lib/Calculator/Operations/ConcreteOperations/tangent.dart
@@ -0,0 +1,20 @@
+import '../Interfaces/operations.dart';
+import '../Interfaces/single_operand_operation.dart';
+import '../Interfaces/number.dart';
+import 'dart:math';
+
+class Cotangent extends SingleOperandOperation {
+  Cotangent(Operation op) : super(op);
+
+  @override
+  Operation operate() {
+    double val = (super.myVal.operate() as Number).getVal();
+    double ret;
+    if (cos(val) == 0) {
+      ret = double.infinity;
+    } else {
+      ret = sin(val) / cos(val);
+    }
+    return Number(ret);
+  }
+}
