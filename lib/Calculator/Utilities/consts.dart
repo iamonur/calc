9a689272ed6b0862935add7f1223ba2495a8f294
Onur Tekik <otekik@gmail.com>
2022-06-11 23:20:48 +0300

    Add consts

---
+++ lib/Calculator/Utilities/consts.dart
@@ -0,0 +1,33 @@
+const String pRight_ = ')',
+    pLeft_ = '(',
+    divide_ = '/',
+    multiply_ = '*',
+    add_ = '+',
+    sub_ = '-',
+    log__ = 'log?',
+    sine_ = 'sin',
+    cosine_ = 'cos',
+    tangent_ = 'tan',
+    ctangent_ = 'ctan',
+    log_ = 'log',
+    ln_ = 'ln';
+
+const List<String> binaryOperators = [multiply_, add_, sub_, divide_, log__];
+
+const List<String> unaryOperators = [
+  sine_,
+  cosine_,
+  tangent_,
+  ctangent_,
+  log_,
+  ln_
+];
+
+const List<String> functionList = [
+  sine_,
+  cosine_,
+  tangent_,
+  ctangent_,
+  log_,
+  ln_
+];
