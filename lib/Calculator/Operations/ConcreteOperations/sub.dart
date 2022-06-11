7bcf457737ec243d4272bcf0f7c413d30899db1f
Onur Tekik <otekik@gmail.com>
2022-06-11 22:44:41 +0300

    Add sub

---
+++ lib/Calculator/Operations/ConcreteOperations/sub.dart
@@ -0,0 +1,14 @@
+import '../Interfaces/operations.dart';
+import '../Interfaces/double_operand_operation.dart';
+import '../Interfaces/number.dart';
+
+class Sub extends DoubleOperandOperation {
+  //TODO: Sub operations with one param should work.
+  Sub(Operation op1, Operation op2) : super(op1, op2);
+
+  @override
+  Operation operate() {
+    return Number((super.myVal1.operate() as Number).getVal() -
+        (super.myVal2.operate() as Number).getVal());
+  }
+}
