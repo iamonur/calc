51e5e7f45ac9ede468abba755e8454cceb8af7b3
Onur Tekik <otekik@gmail.com>
2022-06-11 21:07:11 +0300

    Add add

---
+++ lib/Calculator/Operations/ConcreteOperations/add.dart
@@ -0,0 +1,14 @@
+import 'package:calc/Calculator/Operations/Interfaces/operations.dart';
+
+import '../Interfaces/double_operand_operation.dart';
+import '../Interfaces/number.dart';
+
+class Add extends DoubleOperandOperation {
+  Add(Operation op1, Operation op2) : super(op1, op2);
+
+  @override
+  Operation operate() {
+    return Number((super.myVal1.operate() as Number).getVal() +
+        (super.myVal2.operate() as Number).getVal());
+  }
+}
