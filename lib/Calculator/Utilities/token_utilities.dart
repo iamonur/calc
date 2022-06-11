27e742bce98cd70507efe2f81f904ddcbf51c1e6
Onur Tekik <otekik@gmail.com>
2022-06-12 00:01:38 +0300

    Add token utilities

---
+++ lib/Calculator/Utilities/token_utilities.dart
@@ -0,0 +1,51 @@
+import './consts.dart';
+
+enum prec { none, subAdd, multDiv, funcP }
+
+Map<String, prec> precedences = {
+  sub_: prec.subAdd,
+  add_: prec.subAdd,
+  multiply_: prec.multDiv,
+  divide_: prec.multDiv,
+  pLeft_: prec.funcP,
+  pRight_: prec.funcP
+};
+
+bool isFunction(String op) {
+  return functionList.contains(op);
+}
+
+bool isUnary(String op) {
+  return unaryOperators.contains(op);
+}
+
+bool isBinary(String op) {
+  return binaryOperators.contains(op);
+}
+
+bool isDigit(String token) {
+  RegExp digits =
+      RegExp(r"((?:^|\s)([+-]?[[:digit:]]*\.?[[:digit:]]+)(?=$|\s))");
+  return digits.hasMatch(token);
+}
+
+bool isOperator(String op) {
+  return (isFunction(op) || isUnary(op) || isBinary(op));
+}
+
+prec precedence(String op) {
+  if (isFunction(op)) {
+    return prec.funcP;
+  } else if (precedences.containsKey(op)) {
+    return precedences[op] as prec;
+  } else {
+    return prec.none;
+  }
+}
+
+int compare(String op1, String op2) {
+  if (precedence(op1) == precedence(op2)) {
+    return -1;
+  }
+  return (precedence(op1) as int) > (precedence(op2) as int) ? 1 : -1;
+}
