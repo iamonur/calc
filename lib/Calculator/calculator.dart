d81b457e393b57663b67798c1c46702802af879a
Onur Tekik <otekik@gmail.com>
2022-06-16 23:04:19 +0300

    Fix precision

--- lib/Calculator/calculator.dart
+++ lib/Calculator/calculator.dart
@@ -2,7 +2,6 @@ import './Utilities/expression_tree_builder.dart';
 
 import './Operations/Interfaces/operations.dart';
 import './Operations/Interfaces/number.dart';
-
 import './Utilities/parser.dart';
 
 String calculate(String input) {
@@ -11,6 +10,13 @@ String calculate(String input) {
   Operation a = buildTreeFromRPN(p.getParsedQ());
   Number c = a.operate() as Number;
   double res = c.getVal();
-
+  if (res > 999999999 ||
+      res < -999999999 ||
+      (res < 0.000000001 && res > -0.000000001)) {
+    return res.toStringAsExponential();
+  }
+  if (res.toString().length > 10) {
+    return res.toStringAsPrecision(10);
+  }
   return res.toString();
 }
